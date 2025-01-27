a502f9243fc2396da08047f1a6bab1e5d7a56504
Ben Arzhang <barzhang@gmail.com>
2025-01-27 15:44:33 +0330

    Uses .env File Configurations for API Calls

--- src/configs/api.js
+++ src/configs/api.js
@@ -5,7 +5,7 @@ axios.defaults.withXSRFToken = true;
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8181/',
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
     withCredentials: true,
     headers: {
       Accept: 'application/json',
@@ -28,22 +28,22 @@ apiClient
     return Promise.reject(error);
   });
 
-export const registerApi = registerData => apiClient.post('api/register', registerData);
-export const loginApi = loginData => apiClient.post('api/login', loginData);
-export const logoutApi = () => apiClient.post('api/logout');
+export const registerApi = registerData => apiClient.post('register', registerData);
+export const loginApi = loginData => apiClient.post('login', loginData);
+export const logoutApi = () => apiClient.post('logout');
 
-export const getUserApi = () => apiClient.get('api/user');
+export const getUserApi = () => apiClient.get('user');
 
 export const updateTagApi = (tagId, newTagName) =>
     apiClient.put(
-      `api/tag/${tagId}`,
+      `tag/${tagId}`,
       {name: newTagName}
     );
-export const getTagApi = () => apiClient.get('api/tag');
+export const getTagApi = () => apiClient.get('tag');
 
 
-export const getScenariosApi = () => apiClient.get(`api/scenario`);
-export const getScenarioApi = scenarioId => apiClient.get(`api/scenario/${scenarioId}`);
+export const getScenariosApi = () => apiClient.get(`scenario`);
+export const getScenarioApi = scenarioId => apiClient.get(`scenario/${scenarioId}`);
 export const createScenarioApi = scenarioCreationData =>
   apiClient.post(
     `api/scenario`,
@@ -51,3 +51,5 @@ export const createScenarioApi = scenarioCreationData =>
   );
 export const updateScenarioApi = (scenarioUpdateData, scenarioId) =>
   apiClient.put(`api/scenario/${scenarioId}`, scenarioUpdateData);
+
+export const createTaskApi = scenarioId => apiClient.post('scenario')
